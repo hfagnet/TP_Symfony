7d2e81afb0a920fd54f0cdd0a3b68aff4d94a3c1
Hugo <hugo.fagnet@etudiant.univ-rennes1.fr>
2021-11-22 13:25:40 +0000

    Intruduction à Twig et Creation des donnees

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Faker\Factory;
+use App\Entity\Article;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= 10; $i++)
+        {
+            $article = new Article();
+
+            $sentence = $faker->sentence(4);
+            $title = substr($sentence, 0, strlen($sentence) - 1);
+            $article->setTitle($title)
+                    ->setAuthor($faker->name())
+                    ->setContent($faker->text(3000))
+                    ->setDatetime($faker->dateTimeThisYear());
+
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
